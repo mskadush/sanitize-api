308b1c20cbf4b431e2cc19eb6ae0cfc89b26a162
Minenhle Sithole <minenhle@skadush.co.za>
2024-11-10 22:58:55 +0200

    Connecting to SQL

--- app/src/main/kotlin/com/example/sanitize/adapters/in/internal/InternalSantizationController.kt
+++ app/src/main/kotlin/com/example/sanitize/adapters/in/internal/InternalSantizationController.kt
@@ -3,6 +3,10 @@ package com.example.sanitize.adapters.`in`.internal
 import com.example.sanitize.adapters.`in`.internal.dtos.SensitiveWord
 import com.example.sanitize.adapters.`in`.internal.dtos.UpdateSensitiveWordRequest
 import com.example.sanitize.adapters.`in`.web.dtos.SanitzationResponse
+import com.example.sanitize.domain.ports.`in`.GetSensitiveWordsUseCase
+import com.example.sanitize.domain.ports.`in`.SaveSensitiveWordsUseCase
+import com.example.sanitize.domain.ports.out.GetSensitiveWordsPort
+import com.example.sanitize.domain.ports.out.SaveSensitiveWordsPort
 import io.swagger.v3.oas.annotations.Hidden
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -13,7 +17,10 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("/internal")
-class InternalSantizationController {
+class InternalSantizationController(
+  private val getSensitiveWordsUseCase: GetSensitiveWordsUseCase,
+  private val saveSensitiveWordsUseCase: SaveSensitiveWordsUseCase,
+) {
 
   // TODO: Rest annotations
   // TODO: Swagger annotation
@@ -21,7 +28,7 @@ class InternalSantizationController {
   fun addSensitiveWords(
     @RequestBody request: List<SensitiveWord>
   ): ResponseEntity<Unit> {
-    sensitiveWords.addAll(request.map { it.text })
+    saveSensitiveWordsUseCase.saveSensitiveWords(request.map { it.text })
     return ResponseEntity.noContent().build()
   }
 
@@ -29,14 +36,14 @@ class InternalSantizationController {
   fun removeSensitiveWords(
     @RequestBody request: List<SensitiveWord>
   ): ResponseEntity<Unit> {
-    sensitiveWords.removeIf { it in request.map { it.text } }
+//    sensitiveWords.removeIf { it in request.map { it.text } }
     return ResponseEntity.noContent().build()
   }
 
   @GetMapping("/words")
   fun getSensitiveWords(
   ): ResponseEntity<List<SensitiveWord>> {
-    return ResponseEntity.ok(sensitiveWords.map {
+    return ResponseEntity.ok(getSensitiveWordsUseCase.getSensitiveWords().getOrThrow().map {
       SensitiveWord(text = it)
     })
   }
@@ -45,241 +52,10 @@ class InternalSantizationController {
   fun updateSensitiveWords(
     @RequestBody request: UpdateSensitiveWordRequest
   ): ResponseEntity<Unit> {
-    val removed = sensitiveWords.removeIf { it == request.currentValue.text }
-    if (removed) {
-      sensitiveWords.add(request.newValue.text)
-    }
+//    val removed = sensitiveWords.removeIf { it == request.currentValue.text }
+//    if (removed) {
+//      sensitiveWords.add(request.newValue.text)
+//    }
     return ResponseEntity.noContent().build()
   }
-
-  val sensitiveWords = mutableSetOf(
-    "ACTION"
-    ,"ADD"
-    ,"ALL"
-    ,"ALLOCATE"
-    ,"ALTER"
-    ,"ANY"
-    ,"APPLICATION"
-    ,"ARE"
-    ,"AREA"
-    ,"ASC"
-    ,"ASSERTION"
-    ,"ATOMIC"
-    ,"AUTHORIZATION"
-    ,"AVG"
-    ,"BEGIN"
-    ,"BY"
-    ,"CALL"
-    ,"CASCADE"
-    ,"CASCADED"
-    ,"CATALOG"
-    ,"CHECK"
-    ,"CLOSE"
-    ,"COLUMN"
-    ,"COMMIT"
-    ,"COMPRESS"
-    ,"CONNECT"
-    ,"CONNECTION"
-    ,"CONSTRAINT"
-    ,"CONSTRAINTS"
-    ,"CONTINUE"
-    ,"CONVERT"
-    ,"CORRESPONDING"
-    ,"CREATE"
-    ,"CROSS"
-    ,"CURRENT"
-    ,"CURRENT_PATH"
-    ,"CURRENT_SCHEMA"
-    ,"CURRENT_SCHEMAID"
-    ,"CURRENT_USER"
-    ,"CURRENT_USERID"
-    ,"CURSOR"
-    ,"DATA"
-    ,"DEALLOCATE"
-    ,"DECLARE"
-    ,"DEFAULT"
-    ,"DEFERRABLE"
-    ,"DEFERRED"
-    ,"DELETE"
-    ,"DESC"
-    ,"DESCRIBE"
-    ,"DESCRIPTOR"
-    ,"DETERMINISTIC"
-    ,"DIAGNOSTICS"
-    ,"DIRECTORY"
-    ,"DISCONNECT"
-    ,"DISTINCT"
-    ,"DO"
-    ,"DOMAIN"
-    ,"DOUBLEATTRIBUTE"
-    ,"DROP"
-    ,"EACH"
-    ,"EXCEPT"
-    ,"EXCEPTION"
-    ,"EXEC"
-    ,"EXECUTE"
-    ,"EXTERNAL"
-    ,"FETCH"
-    ,"FLOAT"
-    ,"FOREIGN"
-    ,"FOUND"
-    ,"FULL"
-    ,"FUNCTION"
-    ,"GET"
-    ,"GLOBAL"
-    ,"GO"
-    ,"GOTO"
-    ,"GRANT"
-    ,"GROUP"
-    ,"HANDLER"
-    ,"HAVING"
-    ,"IDENTITY"
-    ,"IMMEDIATE"
-    ,"INDEX"
-    ,"INDEXED"
-    ,"INDICATOR"
-    ,"INITIALLY"
-    ,"INNER"
-    ,"INOUT"
-    ,"INPUT"
-    ,"INSENSITIVE"
-    ,"INSERT"
-    ,"INTERSECT"
-    ,"INTO"
-    ,"ISOLATION"
-    ,"JOIN"
-    ,"KEY"
-    ,"LANGUAGE"
-    ,"LAST"
-    ,"LEAVE"
-    ,"LEVEL"
-    ,"LOCAL"
-    ,"LONGATTRIBUTE"
-    ,"LOOP"
-    ,"MODIFIES"
-    ,"MODULE"
-    ,"NAMES"
-    ,"NATIONAL"
-    ,"NATURAL"
-    ,"NEXT"
-    ,"NULLIF"
-    ,"ON"
-    ,"ONLY"
-    ,"OPEN"
-    ,"OPTION"
-    ,"ORDER"
-    ,"OUT"
-    ,"OUTER"
-    ,"OUTPUT"
-    ,"OVERLAPS"
-    ,"OWNER"
-    ,"PARTIAL"
-    ,"PATH"
-    ,"PRECISION"
-    ,"PREPARE"
-    ,"PRESERVE"
-    ,"PRIMARY"
-    ,"PRIOR"
-    ,"PRIVILEGES"
-    ,"PROCEDURE"
-    ,"PUBLIC"
-    ,"READ"
-    ,"READS"
-    ,"REFERENCES"
-    ,"RELATIVE"
-    ,"REPEAT"
-    ,"RESIGNAL"
-    ,"RESTRICT"
-    ,"RETURN"
-    ,"RETURNS"
-    ,"REVOKE"
-    ,"ROLLBACK"
-    ,"ROUTINE"
-    ,"ROW"
-    ,"ROWS"
-    ,"SCHEMA"
-    ,"SCROLL"
-    ,"SECTION"
-    ,"SELECT"
-    ,"SEQ"
-    ,"SEQUENCE"
-    ,"SESSION"
-    ,"SESSION_USER"
-    ,"SESSION_USERID"
-    ,"SET"
-    ,"SIGNAL"
-    ,"SOME"
-    ,"SPACE"
-    ,"SPECIFIC"
-    ,"SQL"
-    ,"SQLCODE"
-    ,"SQLERROR"
-    ,"SQLEXCEPTION"
-    ,"SQLSTATE"
-    ,"SQLWARNING"
-    ,"STATEMENT"
-    ,"STRINGATTRIBUTE"
-    ,"SUM"
-    ,"SYSACC"
-    ,"SYSHGH"
-    ,"SYSLNK"
-    ,"SYSNIX"
-    ,"SYSTBLDEF"
-    ,"SYSTBLDSC"
-    ,"SYSTBT"
-    ,"SYSTBTATT"
-    ,"SYSTBTDEF"
-    ,"SYSUSR"
-    ,"SYSTEM_USER"
-    ,"SYSVIW"
-    ,"SYSVIWCOL"
-    ,"TABLE"
-    ,"TABLETYPE"
-    ,"TEMPORARY"
-    ,"TRANSACTION"
-    ,"TRANSLATE"
-    ,"TRANSLATION"
-    ,"TRIGGER"
-    ,"UNDO"
-    ,"UNION"
-    ,"UNIQUE"
-    ,"UNTIL"
-    ,"UPDATE"
-    ,"USAGE"
-    ,"USER"
-    ,"USING"
-    ,"VALUE"
-    ,"VALUES"
-    ,"VIEW"
-    ,"WHERE"
-    ,"WHILE"
-    ,"WITH"
-    ,"WORK"
-    ,"WRITE"
-    ,"ALLSCHEMAS"
-    ,"ALLTABLES"
-    ,"ALLVIEWS"
-    ,"ALLVIEWTEXTS"
-    ,"ALLCOLUMNS"
-    ,"ALLINDEXES"
-    ,"ALLINDEXCOLS"
-    ,"ALLUSERS"
-    ,"ALLTBTS"
-    ,"TABLEPRIVILEGES"
-    ,"TBTPRIVILEGES"
-    ,"MYSCHEMAS"
-    ,"MYTABLES"
-    ,"MYTBTS"
-    ,"MYVIEWS"
-    ,"SCHEMAVIEWS"
-    ,"DUAL"
-    ,"SCHEMAPRIVILEGES"
-    ,"SCHEMATABLES"
-    ,"STATISTICS"
-    ,"USRTBL"
-    ,"STRINGTABLE"
-    ,"LONGTABLE"
-    ,"DOUBLETABLE"
-    ,"SELECT * FROM"
-  )
 }
---
+++ app/src/main/kotlin/com/example/sanitize/domain/services/SanitizationService.kt
@@ -0,0 +1,22 @@
+package com.example.sanitize.domain.services
+
+import com.example.sanitize.domain.ports.`in`.GetSensitiveWordsUseCase
+import com.example.sanitize.domain.ports.`in`.SaveSensitiveWordsUseCase
+import com.example.sanitize.domain.ports.out.GetSensitiveWordsPort
+import com.example.sanitize.domain.ports.out.SaveSensitiveWordsPort
+import org.springframework.stereotype.Component
+
+@Component
+class SanitizationService(
+  private val getSensitiveWordsPort: GetSensitiveWordsPort,
+  private val saveSensitiveWordsPort: SaveSensitiveWordsPort,
+): SaveSensitiveWordsUseCase, GetSensitiveWordsUseCase {
+  override fun saveSensitiveWords(words: List<String>): Result<Unit> {
+    return saveSensitiveWordsPort.saveSensitiveWords(words)
+  }
+
+  override fun getSensitiveWords(): Result<List<String>> {
+   return getSensitiveWordsPort.getSensitiveWords()
+  }
+
+}
