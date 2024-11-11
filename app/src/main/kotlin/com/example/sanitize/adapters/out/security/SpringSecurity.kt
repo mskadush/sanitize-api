0853f94f9404c95c15c5adcc673241b5e8b61ded
Minenhle Sithole <minenhle@skadush.co.za>
2024-11-11 22:06:21 +0200

    Adding auth rules

--- app/src/main/kotlin/com/example/sanitize/adapters/out/security/SpringSecurity.kt
+++ app/src/main/kotlin/com/example/sanitize/adapters/out/security/SpringSecurity.kt
@@ -2,10 +2,14 @@ package com.example.sanitize.adapters.out.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authorization.AuthorizationDecision
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext
+import java.util.function.Supplier
 
 @Configuration
 @EnableWebSecurity
@@ -18,7 +22,9 @@ class SecurityConfig {
         disable() // disable since it causes 403s and we are not gonna use sessions
       }
       authorizeHttpRequests {
-        authorize("/**", permitAll)
+        authorize("/sanitize") { supplier: Supplier<Authentication>, ctx: RequestAuthorizationContext ->
+          AuthorizationDecision(true)
+        }
       }
     }
     return http.build()
