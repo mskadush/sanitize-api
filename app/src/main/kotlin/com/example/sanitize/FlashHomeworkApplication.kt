18e5f66dabf274627b972cf309534ca898e3af69
Minenhle Sithole <minenhle@skadush.co.za>
2024-11-12 22:01:33 +0200

    Security documenting

--- app/src/main/kotlin/com/example/sanitize/FlashHomeworkApplication.kt
+++ app/src/main/kotlin/com/example/sanitize/FlashHomeworkApplication.kt
@@ -1,8 +1,18 @@
 package com.example.sanitize
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
+@OpenAPIDefinition(
+  servers = [
+    Server(url = "https://localhost:8080", description = "HTTPS endpoint"),
+    Server(url = "http://localhost:8080", description = "HTTP endpoint"),
+  ],
+  security = [ // TODO:
+  ]
+)
 @SpringBootApplication
 class FlashHomeworkApplication
 
