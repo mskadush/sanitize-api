e8856f856771ce367350b2c16cf2672756922739
Minenhle Sithole <minenhle@skadush.co.za>
2024-11-12 10:02:07 +0200

    Fixing migration

--- app/src/main/kotlin/com/example/sanitize/adapters/out/security/ControllerAdvisor.kt
+++ app/src/main/kotlin/com/example/sanitize/adapters/out/security/ControllerAdvisor.kt
@@ -8,7 +8,7 @@ import java.time.LocalDateTime
 
 
 @RestControllerAdvice
-class ControllerAdvisor{
+class ControllerAdvisor {
 
   @ExceptionHandler(Throwable::class)
   fun generalException(
--- app/src/main/resources/db/migration/V1__boostrap_words.sql
+++ app/src/main/resources/db/migration/V1__boostrap_words.sql
@@ -1,10 +1,19 @@
 -- Insert initial data into your tables
-CREATE TABLE sensitive_word_model
-(
-    id         BIGINT IDENTITY(1,1) PRIMARY KEY,
-    text       VARCHAR(255) NOT NULL,
-    is_deleted BIT DEFAULT 0,
-);
+-- V1__create_table_if_not_exists.sql
+
+IF NOT EXISTS (SELECT *
+               FROM INFORMATION_SCHEMA.TABLES
+               WHERE TABLE_NAME = 'sensitive_word_model')
+    BEGIN
+        CREATE TABLE sensitive_word_model
+        (
+            id         BIGINT IDENTITY (1,1) PRIMARY KEY,
+            text       VARCHAR(255) NOT NULL,
+            is_deleted BIT DEFAULT 0,
+        );
+    END;
+
+
 INSERT INTO sensitive_word_model (text)
 VALUES ('"ACTION"'),
        ('"ADD"'),
