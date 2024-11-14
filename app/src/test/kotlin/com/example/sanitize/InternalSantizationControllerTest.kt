9ff288e39c429cdf0576104d50f5d0511369a0b2
Minenhle Sithole <minenhle@skadush.co.za>
2024-11-14 08:21:10 +0200

    Running test rest tests

--- app/src/test/kotlin/com/example/sanitize/InternalSantizationControllerTest.kt
+++ app/src/test/kotlin/com/example/sanitize/InternalSantizationControllerTest.kt
@@ -1,5 +1,6 @@
 package com.example.sanitize
 
+import com.example.sanitize.adapters.`in`.internal.dtos.CreateSensitiveWordRequest
 import com.example.sanitize.adapters.`in`.internal.dtos.SensitiveWordDto
 import com.example.sanitize.adapters.`in`.internal.dtos.SensitiveWordDto.Companion.toSensitiveWordDto
 import com.example.sanitize.domain.ports.out.GetSensitiveWordsPort
@@ -42,6 +43,17 @@ class InternalSantizationControllerTest(
 
     result.statusCode shouldBe HttpStatus.OK
     result.body!! shouldBe getSensitiveWordsPort.getAllSensitiveWords().getOrThrow().map { it.toSensitiveWordDto() }
+  }
+
+  @Test
+  fun `addSensitiveWords - success when api key provided`() {
+    val numWords = getSensitiveWordsPort.getAllSensitiveWords().getOrThrow().size
+    val result = restTemplate.exchange<List<SensitiveWordDto>>(RequestEntity.post("/internal/words").headers(HttpHeaders().apply {
+      add("X-Internal-Api-Key", "test-internal-key")
+    }).body(listOf(CreateSensitiveWordRequest(text = "added"))))
+
+    result.statusCode shouldBe HttpStatus.NO_CONTENT
+    getSensitiveWordsPort.getAllSensitiveWords().getOrThrow()[numWords].text shouldBe "added"
   }
 
 }
